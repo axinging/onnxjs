45a2565d9288e3a8278cd1a898d8d8b67af1627d
Xu Xing <xing.xu@intel.com>
2023-10-10 11:13:16 +0800

    Add example node

---
+++ example-node/index.js
@@ -0,0 +1,12 @@
+require('../test/onnx.dev.js');
+//require('../dist/onnx.min.js');
+// Use package 'onnxjs-node' to load ONNXRuntime backend.
+
+const assert = require('assert');
+
+async function main() {
+  // Create an ONNX inference session with ONNXRuntime backend. 
+   console.log(onnx);
+}
+
+main();
